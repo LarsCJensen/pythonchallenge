1cf761503792e783fb06f34e162afb188676079e
larscjensen <lars.c.jensen@gmail.com>
2021-12-07 18:12:10 +0100

    4 done

---
+++ 4.py
@@ -0,0 +1,27 @@
+import urllib.request
+
+
+def _get_next_id(resp: str) -> str:
+    temp_list = resp.split(" ")
+    return temp_list[len(temp_list) - 1]
+
+
+quirks = ["There maybe misleading numbers in the text.", "Your hands are getting tired"]
+
+url_base = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing="
+id = "12345"
+for i in range(399):
+    resp = urllib.request.urlopen(url_base + id).read().decode("utf-8")
+    if resp == "Yes. Divide by two and keep going.":
+        id = str(int(id) / 2)
+    elif (
+        "There maybe misleading numbers in the text." in resp
+        or "Your hands are getting tired" in resp
+    ):
+        id = _get_next_id(resp)
+    else:
+        if "the next nothing" not in resp:
+            break
+        id = _get_next_id(resp)
+
+print(resp)
